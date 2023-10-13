dee9cf3fb0ed38682df340a319416412e01febbb
Nathan Trautenberg <Natester123@mac.com>
2023-10-13 16:57:40 -0600

    update index files to display attribute names when looking at the server page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+avianorder_1 = AvianOrder.create!(order: "passeriformes",
+                        families: "Muscicapoidea, Fringillidae, Troglodytidae",
+                         species: 6500,
+                         anisodactyl: true,
+                         zygodactyl: false,
+                         tridactyl: false,
+                         didactyl: false)
+avianorder_2 = AvianOrder.create!(order: "strigiformes",
+        families: "Strigidae, Tytonidae",
+        species: 225,
+        anisodactyl: false,
+        zygodactyl: true,
+        tridactyl: false,
+        didactyl: false)
+bird_1 = avianorder_1.birds.create!(name: "Common starling",
+                        population: 310000000,
+                         migratory: true,
+                         sexual_dichromatism: true)
+bird_2 = avianorder_2.birds.create!(name: "Great horned owl",
+                         population: 5700000,
+                          migratory: false,
+                          sexual_dichromatism: false)
