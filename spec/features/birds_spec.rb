5bd6bf63ee5680ec1d39e8595de7c313a9b69920
Nathan Trautenberg <Natester123@mac.com>
2023-10-16 20:25:44 -0600

    add link to bird index page from all pages on site.

--- spec/features/birds_spec.rb
+++ spec/features/birds_spec.rb
@@ -1,6 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "AvianOrders Index", type: :feature do
+  before :each do
+    @avianorder_1 = AvianOrder.create!(order: "passeriformes",
+                        families: "Muscicapoidea, Fringillidae, Troglodytidae",
+                         species: 6500,
+                         anisodactyl: true,
+                         zygodactyl: false,
+                         tridactyl: false,
+                         didactyl: false)
+    @bird_1 = @avianorder_1.birds.create!(name: "Common starling",
+                        population: 310000000,
+                         migratory: true,
+                         sexual_dichromatism: true)
+  end
 
   # As a user,
   # When I visit '/avianorders'
@@ -27,6 +40,19 @@ RSpec.describe "AvianOrders Index", type: :feature do
         expect(page).to have_content("Birds")
         expect(page).to have_content(bird_1.name)
         expect(page).to have_content(bird_1.population)
+      end
+      # User Story 8, Child Index Link
+      # As a visitor
+      # When I visit any page on the site
+      # Then I see a link at the top of the page that takes me to the Child Index
+      it "will add a link to birds index from any page on the site" do
+        visit "/birds"
+        click_link('List of Birds')
+        expect(current_path).to eq('/birds')
+
+        visit "/avianorders"
+        click_link('List of Birds')
+        expect(current_path).to eq('/birds')
       end
     end
   end
